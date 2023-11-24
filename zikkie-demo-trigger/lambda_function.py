8d8b757379f42325fbaa9f006c7fb68f20a6b483
Jesufemi-O <ogunwedeemmanuel@gmail.com>
2023-11-24 17:45:43 +0100

    lint 😑

--- zikkie-demo-trigger/lambda_function.py
+++ zikkie-demo-trigger/lambda_function.py
@@ -4,7 +4,7 @@ import urllib.parse
 
 
 def lambda_handler(event, context):
-    print(f"executing lambda for payload: {event}")
+    print(f"executing lambda for payload: {event} and context {context}")
     bucket = event["Records"][0]["s3"]["bucket"]["name"]
     key = urllib.parse.unquote_plus(event["Records"][0]["s3"]["object"]["key"], encoding="utf-8")
     print(f"Make API call to orchestrator asking it to process file {key} from bucket {bucket}")
