1a22186c173c53783b34926d7c7322413f8af438
Jesufemi-O <ogunwedeemmanuel@gmail.com>
2023-11-24 17:39:36 +0100

    black sucks tbh

--- zikkie-demo-trigger/lambda_function.py
+++ zikkie-demo-trigger/lambda_function.py
@@ -6,11 +6,6 @@ import urllib.parse
 def lambda_handler(event, context):
     print(f"executing lambda for payload: {event}")
     bucket = event["Records"][0]["s3"]["bucket"]["name"]
-    key = urllib.parse.unquote_plus(
-        event["Records"][0]["s3"]["object"]["key"], encoding="utf-8"
-    )
+    key = urllib.parse.unquote_plus(event["Records"][0]["s3"]["object"]["key"], encoding="utf-8")
     print(f"Make API call to orchestrator asking it to process file {key} from bucket {bucket}")
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Successfully submitted job to Orchestrator!!')
-    }
+    return {"statusCode": 200, "body": json.dumps("Successfully submitted job to Orchestrator!!")}
